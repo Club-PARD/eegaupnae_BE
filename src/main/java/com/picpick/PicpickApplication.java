c228e052ea1ba85a3aa0a1938afb6fffcce5da2a
정성국 <ssoup0224@gmail.com>
2025-12-26 16:05:59 +0900

    Initial commit: Spring Boot 3.5.8 with Google GenAI

---
+++ src/main/java/com/picpick/PicpickApplication.java
@@ -0,0 +1,13 @@
+package com.picpick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicpickApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/picpick/PicpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpickApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
