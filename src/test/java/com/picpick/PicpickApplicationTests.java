38337963967d94b90dcb073eae5d98b4a533c2e2
정성국 <ssoup0224@gmail.com>
2026-01-09 06:50:36 +0900

    readded test

---
+++ src/test/java/com/picpick/PicpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class PicpickApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
