425ed0e1ca572580cb379e4692cb7cb3c51d4d15
정성국 <ssoup0224@gmail.com>
2026-01-09 18:03:27 +0900

    revert getScans changes

--- src/main/java/com/picpick/scan/ScanService.java
+++ src/main/java/com/picpick/scan/ScanService.java
@@ -97,10 +97,7 @@ public class ScanService {
         List<Scan> scans = scanRepository.findAllByUser_Id(userId);
 
         // Update isShown to true when fetched
-        scans.forEach(scan -> {
-            log.info("Scan retrieved: {}, naverImage: {}", scan.getScanName(), scan.getNaverImage());
-            scan.setIsShown(true);
-        });
+        scans.forEach(scan -> scan.setIsShown(true));
 
         // Synchronously try to reuse existing analysis for scans that don't have one
         List<Scan> scansToAnalyze = scans.stream()
